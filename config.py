9f80b550d39fe5a3eef3d3e2673bcc5045357a91
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-01-12 20:13:16 +0530

    Separated gmail search filter into config

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 GOOGLE_CLIENT_SECRETS_JSON='client_secret.json'                # path to `client_secret.json`
 REGISTERED_CREDENTIALS_JSON='.registered_credentials.json'     # path to `.registered_credentials.json`
+GMAIL_FILTER_QUERY='subject:(lost) OR subject:(found)'
